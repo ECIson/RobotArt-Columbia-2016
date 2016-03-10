c12f328d6378e5c0b43df96fa74153d0fe3befd5
Elliott C Ison <eci2109@santiago.clic.cs.columbia.edu>
2016-03-10 16:43:37 -0500

    generated ROS catkin workspace

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/eci2109/Desktop/robotart/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/eci2109/Desktop/robotart/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/eci2109/Desktop/robotart/devel/lib:/home/eci2109/Desktop/robotart/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib"
+export PATH="/home/eci2109/Desktop/robotart/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/eci2109/Desktop/robotart/devel/lib/pkgconfig:/home/eci2109/Desktop/robotart/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/eci2109/Desktop/robotart/build"
+export PYTHONPATH="/home/eci2109/Desktop/robotart/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/eci2109/Desktop/robotart/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/eci2109/Desktop/robotart/src:$ROS_PACKAGE_PATH"
