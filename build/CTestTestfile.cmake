777123c81d8a2b262f546c6dce56baaeb148253d
Elliott C Ison <eci2109@santiago.clic.cs.columbia.edu>
2016-03-10 16:54:49 -0500

    removed workspaced

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/eci2109/Desktop/robotart/src
-# Build directory: /home/eci2109/Desktop/robotart/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
