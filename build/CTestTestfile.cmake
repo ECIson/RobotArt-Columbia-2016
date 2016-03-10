86c742d4a987b54c3b1a2c77f881592d32276319
Elliott C Ison <eci2109@santiago.clic.cs.columbia.edu>
2016-03-10 16:59:11 -0500

    trying to make workspace again

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/eci2109/robotart_ws/src
+# Build directory: /home/eci2109/robotart_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/eci2109/robotart_ws/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/eci2109/robotart_ws/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/eci2109/robotart_ws/devel/lib:/home/eci2109/robotart_ws/devel/lib/x86_64-linux-gnu:/home/eci2109/Desktop/robotart/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/home/eci2109/Desktop/robotart/devel/lib:/opt/ros/indigo/lib"
+export PATH="/home/eci2109/robotart_ws/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/eci2109/robotart_ws/devel/lib/pkgconfig:/home/eci2109/robotart_ws/devel/lib/x86_64-linux-gnu/pkgconfig:/home/eci2109/Desktop/robotart/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/home/eci2109/Desktop/robotart/devel/lib/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export PWD="/home/eci2109/robotart_ws/build"
+export PYTHONPATH="/home/eci2109/robotart_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/eci2109/robotart_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/eci2109/robotart_ws/src:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
